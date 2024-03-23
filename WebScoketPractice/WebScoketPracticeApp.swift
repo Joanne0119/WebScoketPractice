c8e3f40ec5bcf27f31556ce5c478106123aecbff
Joanne <liuchengen>
2024-03-23 21:12:58 +0800

    Initial Commit

---
+++ WebScoketPractice/WebScoketPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebScoketPracticeApp.swift
+//  WebScoketPractice
+//
+//  Created by 劉丞恩 on 2024/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WebScoketPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
